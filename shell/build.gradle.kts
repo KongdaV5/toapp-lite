6e6e89b11e775fd903e873e39457ee0ccbc80ad1
Kongda <37110875+KongdaV5@users.noreply.github.com>
2026-08-04 09:25:45 +0800

    Downgrade compileSdk and targetSdk to version 36

--- shell/build.gradle.kts
+++ shell/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.kongda.toapplite.shell"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.toapplite.generated.placeholderaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
